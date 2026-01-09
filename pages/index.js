6d84d82f656aaa651d9ccb3c35e63a16f30a925e
Marco Antonio Barro Tortelli <marcobarrotortelli@gmail.com>
2026-01-08 23:39:46 -0300

    add .gitignore

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <h1>
+            A história se repete, a primeira vez como tragédia e a segunda como
+            farsa
+        </h1>
+    );
+}
+export default Home;
