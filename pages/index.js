8c9d5948d5bf3ca12255cdb6636652772fc88d00
Marco Antonio Barro Tortelli <marcobarrotortelli@gmail.com>
2026-01-09 16:22:50 -0300

    mudou a frase

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <h1>
             A história se repete, a primeira vez como tragédia e a segunda como
-            farsa
+            farsa!
         </h1>
     );
 }
