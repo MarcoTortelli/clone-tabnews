47098ee960d0e80fb37d8c78f5caecbace7bf8e4
Marco Antonio Barro Tortelli <marcobarrotortelli@gmail.com>
2026-01-12 00:20:36 -0300

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 function Home() {
-    return (
-        <h1>
-            A história se repete, a primeira vez como tragédia e a segunda como
-            farsa!
-        </h1>
-    );
+  return (
+    <h1>
+      A história se repete, a primeira vez como tragédia e a segunda como farsa!
+    </h1>
+  );
 }
 export default Home;
